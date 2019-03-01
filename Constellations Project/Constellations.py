606d636f1bc8898a2389daa2c062f50ea56f5052
kcarullo <35302366+kcarullo@users.noreply.github.com>
2019-03-01 16:34:48 -0500

    Update Constellations.py

--- Constellations Project/Constellations.py
+++ Constellations Project/Constellations.py
@@ -22,7 +22,7 @@ z = [2.06, 0.84, 1.56, 2.07, 2.36, 1.72, 0.66, 1.25,1.38]
 
 # In[15]:
 
-
+#Create a 2D visualization 
 fig = plt.figure()
 fig.add_subplot(1,1,1)
 plt.scatter(x,y,color='red', marker='*')
@@ -32,11 +32,10 @@ plt.show()
 
 # In[17]:
 
-
+#Create a 3D visualization
 fig_3d = plt.figure()
-projections="3d"
-fig_3d.add_subplot(111,projection="3d")
-constellation3d = plt.scatter(x,y,z,color='orange', marker='*')
+constellation3d = fig_3d.add_subplot(1,1,1,projection="3d")
+constellation3d.scatter(x,y,z, marker='*', color='orange')
 plt.title('Orion 3D Constellation')
 plt.show()
 
